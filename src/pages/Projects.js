78bd2a6286693e8399bc4a4158c08dbdbe155168
KajetanJedrych <jedrychk@gmail.com>
2025-04-22 08:01:02 +0200

    change demo link

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       technologies: ["React", "Tailwind CSS", "TypeScript", "Next.js", "GeminiAi"],
       description: "A website that exports and analyzes YouTube comments using Gemini to generate summaries.",
       githubLink: "https://github.com/KajetanJedrych/summaryai",
-      demoLink: "https://github.com/KajetanJedrych/summaryai"
+      demoLink: "https://www.jedrychk.com/blog/1"
     },
     {
       title: "Calculator Page",
